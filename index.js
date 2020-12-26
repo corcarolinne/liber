e5e8dd2a64df4785731ffee677d64e9c5ac55fd9
corcarolinne <carolinnemcordeiro@gmail.com>
2020-12-26 18:11:40 +0000

    setting up node and express

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
-// first file to execute
- console.log('hello world');
+// index.js
+
+/**
+ * Required External Modules
+ */
+const express = require("express");
+const path = require("path");
+/**
+ * App Variables
+ */
+
+/**
+ *  App Configuration
+ */
+const app = express();
+const port = process.env.PORT || "8000";
+/**
+ * Routes Definitions
+ */
+app.get("/", (req, res) => {
+  res.status(200).send("Liber - Your Online Library");
+});
+/**
+ * Server Activation
+ */
+app.listen(port, () => {
+  console.log(`Listening to requests on http://localhost:${port}`);
+});
